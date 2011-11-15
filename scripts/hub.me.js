b22aa97ea0ecf42938e4c971eb322567cba83af9
Zeno Rocha <contato@zenorocha.com>
2011-11-15 19:53:43 -0200

    Creating calls for Github's API to get the list of all repositories and then sorting it by languages

--- scripts/hub.me.js
+++ scripts/hub.me.js
@@ -1,6 +1,7 @@
 /*
  *  Project: Hub.me
  *  Description: An alternative and responsive gallery for your projects on Github
+ *  URL: https://github.com/zenorocha/hub.me 
  *  Author: Zeno Rocha
  *  License: WTFPL - http://sam.zoy.org/wtfpl/COPYING
  */
@@ -19,9 +20,11 @@
     // minified (especially when both are regularly referenced in your plugin).
 
     // Create the defaults once
-    var pluginName = 'defaultPluginName',
+    var hubMe = 'defaultHubMe',
         defaults = {
-            propertyName: "value"
+            propertyName: 'value',
+            username: 'github',
+            //sortBy: 'language' //popularity 
         };
 
     // The actual plugin constructor
@@ -35,23 +38,90 @@
         this.options = $.extend( {}, defaults, options) ;
         
         this._defaults = defaults;
-        this._name = pluginName;
+        this._name = hubMe;
         
         this.init();
     }
 
     Plugin.prototype.init = function () {
-        // Place initialization logic here
-        // You already have access to the DOM element and the options via the instance, 
-        // e.g., this.element and this.options
+        this.getRepos();
     };
+
+    Plugin.prototype.getRepos = function () {
+
+    	var repos = [];
+
+        $.getJSON('https://api.github.com/users/' + this._defaults.username + '/repos', function (result) {
+			
+			$.each(result, function(i, field) {
+	    		if (field.language != null)
+	    			repos.push(field);
+			});
+
+			repos = this.orderByLanguages(repos);
+
+			$.each(repos, function(i, field) {
+	    		
+	    		if (i > 0) {
+					if (repos[i].language != repos[i-1].language) {
+		    			createCategory(repos[i].language);
+		    		}	    			
+	    		}
+	    		else {
+	    			createCategory(repos[i].language);
+	    		}
+
+	    		createRepo(repos[i]);
+
+			});
+
+        });
+    };
+
+    var orderByLanguages = function() {
+    	return repos.sort(function(a, b) {
+				
+				var langA = a.language, 
+					langB = b.language;
+
+			 	if (langA < langB)
+			  		return -1;
+			 	if (langA > langB)
+			  		return 1;
+
+			});
+    }
+
+    	var createCategory = function(catName) {
+			
+			var cat = '<div class="category">' +
+							'<h2>' + catName + '</h2>' +
+					   '</div>';
+
+			$('body').append(cat);
+
+		}
+
+		var createRepo = function(repo) {
+			
+			var repository = '<a href="' + repo.html_url + '" class="title">' +
+								'<div>' +
+									'<h1>' + repo.name + '</h1>' +
+									'<p>' + repo.description + '</p>' +
+									'<small>' + repo.homepage + '</small>' +
+								'</div>' +
+							'</a>';	
+
+			$('body').append(repository);
+			
+		}
 
     // A really lightweight plugin wrapper around the constructor, 
     // preventing against multiple instantiations
-    $.fn[pluginName] = function ( options ) {
+    $.fn.hubMe = function ( options ) {
         return this.each(function () {
-            if (!$.data(this, 'plugin_' + pluginName)) {
-                $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+            if (!$.data(this, 'plugin_' + hubMe)) {
+                $.data(this, 'plugin_' + hubMe, new Plugin( this, options ));
             }
         });
     }
