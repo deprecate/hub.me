90b58be0005e0d12b239d2baa6eac3adb497f425
Ruan Mer <ruanrm@gmail.com>
2011-12-01 17:59:47 -0200

    fixed repo homepage link

--- scripts/hub.me.js
+++ scripts/hub.me.js
@@ -96,12 +96,14 @@
     };
 
     Plugin.prototype.createRepo = function(repo) {
+        // test if homepage begins with http or https
+        repo.homepage = ( repo.homepage && !( /^(http|https):\/\//.test( repo.homepage ) ) ) ? "http://" + repo.homepage : repo.homepage;          
 
         var repository =    '<article>' +
                                 '<div>' +
                                     '<h2><a href="' + repo.html_url + '">' + repo.name + '</a></h2>' +
                                     '<p>' + repo.description + '</p>' +
-                                    '<a href="' + repo.homepage + '">' + repo.homepage + '</a>' +
+                                    '<a href="' + repo.homepage + '" target="_blank">' + repo.homepage + '</a>' +
                                 '</div>' +
                             '</article>'; 
 
